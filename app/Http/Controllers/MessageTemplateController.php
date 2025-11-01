493b1c3eabb977b97decde07a3ecf8ca5f72d531
lord-aizen-cyber <adam020920@gmail.com>
2025-11-01 19:33:41 +0700

    Upload semua file website

---
+++ app/Http/Controllers/MessageTemplateController.php
@@ -0,0 +1,33 @@
+<?php //00388
+// IONCUBE ENCODER 14.0 EVALUATION
+// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
+// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+
+if(extension_loaded('ionCube Loader')){$die = 'The file '.__FILE__." is corrupted.\n";}$echo=0;$echo="\nScript error: the ".(($cli=(@php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n";$°¸ØØØ = "ihwnrax_4 Tm+cjbuospkRflvgtH6ezyqd";$¶ˆØØµ³ = "9ukwoa15y3gs7c6lx0zfnrtZbmpi4vqe8dhj2";$Ø·²Ø«ŠØ = __FILE__;$¶ØØ = $°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[36]].$°¸ØØØ[$¶ˆØØµ³[17]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]];$ØŠ´Ù´Ù²º = $°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[9].$¶ˆØØµ³[17]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[6]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]];$ÙØÙÙ¸ÙÙ¨Ø = $°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[9].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]];$ØØªÙØØº‡Ù¹° = $°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[28]];$ØØµÙ°ÙÙØØØÙÙ = $°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]];$³´ØØÙ³‡ÙØØ…Ù = $°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[17]].$°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]];$Ø·ØØØªØØØØØÙÙØ = $°¸ØØØ[$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]];$ØØÙ«¬§ˆØÙ°ÙØ³³Ø = $°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[9].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[9].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]];$ÙØÙ­²ÙØØØØØ§¯³‚Ø = $°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[32]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[14]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[28]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[0]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[7]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[0]];$¶ØÙ´Ø‚ØØºØØ±Ù´µØØ = $°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[12]].$°¸ØØØ[$¶ˆØØµ³[36].$¶ˆØØµ³[6]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[36]].$°¸ØØØ[$¶ˆØØµ³[6].$¶ˆØØµ³[9]];$²³ØØÙªØÙÙØ = $¶ØØ($Ø·²Ø«ŠØ);$¹Ù¶Ø = $ÙØÙÙ¸ÙÙ¨Ø($¶ØÙ´Ø‚ØØºØØ±Ù´µØØ, $²³ØØÙªØÙÙØ);$³´ØØÙ³‡ÙØØ…Ù($¹Ù¶Ø[0], $ØØµÙ°ÙÙØØØÙÙ) ? $Ø·ØØØªØØØØØÙÙØ : _;$ÙØÙÙ¸ÙÙ¨Ø = $¶ØÙ´Ø‚ØØºØØ±Ù´µØØ;eval($ØŠ´Ù´Ù²º($ØØªÙØØº‡Ù¹°($ØŠ´Ù´Ù²º($ØØªÙØØº‡Ù¹°($ØØÙ«¬§ˆØÙ°ÙØ³³Ø($ÙØÙ­²ÙØØØØØ§¯³‚Ø("\n", "", $¹Ù¶Ø[1])))))));
+?>
+HR+cjsu9+nja7VnbbttGEH2OAf/DBlBBEqANJ0BeZEiF4QuaoLEL22gfLENYkytpY4rL7i5lC43/
+vbMX3hldnCiWiwoBIpMzs2dnz8wcirs7uzsxnhKR4ICgoyQZ/CZlMjhmseQsiggXh7tgkgpz8zML
+SSQGn4kQeEyuyTSJsCSHDYMTMqNBdv1jFKVTGoOhCX5J/k6JkM27V2mSMC4HZzjAIRGD39l4udGf
+OKIhloxroEGEhUA1fMVuEHmUJA4FKi7t7vyzu4Pgk6R3EQ3QKI0DSVmMaBySR9eCRR1uvnjG2Pqo
+T0faZQTq5WZ7fYDNXW+vP61iEa5XuKrPXv9hQmK3cBQE82DiF0DcDtzhcx91zC2vvHr24USmPEbG
+VMfkxHXU0To+ciJ6r///xUH7WRj4Bn97zVAGFeN/mRiQ5IDTREFZEuqwGutp2U7lPCGt+1Q31til
+Mncyt3VBBHAoY6aWbQGS3VwDTObilN3XBSUklqlo54C+tQYg4+AUrkvBaJ7KJlETPNY16L478HS1
+FSUAmVf0rxVetzsm8miGaYTvInKtjNzy8lmC6HLn49zSra5tNz2j5AGYMCFTcASgROxndbmvKxkS
+ELAptDkJZlkxKiJr7OpLAcbxMpRP2VL17hBwojKxSnvY6tyYfVSS86x8COjAq6QDenq3S+MRc51s
+M+hK+SLr21VMzQsBR5HrldNSStAs6/yQn3wKdLtTfE/cWgQf3VTJbDoj6vWRoywpJ+FXITmNx1+n
++LH7/sMHx6956C5T9aBxV8JE8e9SKVnsR1RIf0pCiv2IQfYgL/4swDz0haTBPeF+wlmYBrIR2w6J
+WvgvQrXcmmneX7RtnEaaAmXw7w4OGl6lJg5pb/Vt+NjG0dgysITOiA+tQH9xCrfbKv3oCPpWfkp7
+/RHwFSja6F2aE4RzxkuksCequHUGfiTUxChF0w5CjVjJjjjH8ypRSvznIG9YLIiyVTl1b5q909Hh
+9GYlT4nfYlE+pHYgI8qbffO2jOqpnB/J5/VUdOwiJ1hioLVCOwxJAKqqRGlr42uk9T2rpGs3UEJy
+aLLqobe9Hvp0dXE+PL28vLgcnl+cny4aIatkbLWsNTI3HLrOx1inD9nraMT4FMt99EdEMKi9YEKC
+ezRnKQcRlqRyv02l3DbmWDm1FWG2oi4DLWCaehs98n5RRFKX2niSN4qa0llGqdLRt9lWCr9Fv7S4
+lKVb1avaD9p8S9Vf1yUt/G4p6FqTN7kNkUiDADY6guYzVyV9k0/jIQ3NctmFvT4NW6I/r6pHOBJL
+z0CRcyHit47XeuTWp4p/YZ6eEJwcSGd3cPoYEH0SqENW640amXIYmc6ItBaHfIEqsEqhZeaZhGSL
+GajG55rD09+RuNJzwG3doaXAEHoerpFCT9gFPW+znVgdmRkQSDJ7ZiiXN1lLUa0WjzGNG72k3p4X
+q5wJe2iIHBhLNFzwXLhy+xmBUL3gajOuinjYEHLN9H2DebdL9RqoFPkSO9lyOazS0vak8PwHhTQJ
+2x4UNpjq/yXyf08iv0IlW0xxWwIvI6tWkdCvXG79LEFkznEbBZFBtlAOvZQisdA2pUiAWZKz+arz
+ZVGh/oDpXiv8kEQECv/lGGsANBi7MR7q9ZYJ85dioknGxpiYcKKV1RYxMWCxhAkM4Vx294UE0oPH
+zZaRA6dTvCv5xCYxOmHE8W5r8WrR7YbtuSrMBfMh4pn+UQPYlx18BsdbGPYHl4YFaTZcRbwFfdsC
+2spyybDl9fJd1SHZeByRKz3Qt7NZL1BpZXFSOJirqNfrIcfqXPQrcnLRi7r59a2RMBbzGkpm0d43
+VhcWpiHNa5A1FvH31Yj6JSqjLnTPYwwP4XQc/4x38Ian6vqbN2++/QZUwAhVPwvQUE2KbGLY98/l
+V7955hoh1Kxp/Vli2ZEt4X3ppWaFq6L27GlPAP79C87nUvT6UgWtAdp4
